b2edc63b5428690a8f5e69e0d4f088c7dddc49f7
Srushti Dhakate <86475735+srushti-dhakate-2210@users.noreply.github.com>
2023-07-08 14:49:20 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -19,7 +19,11 @@ const app = express();
 // middleware
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+
+app.use(cors({
+    origin: '*'
+}));
+
 // to serve images inside public folder
 app.use(express.static('public')); 
 app.use('/images', express.static('images'));
