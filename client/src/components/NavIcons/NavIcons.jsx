ac1a5ac954c149b3906dcc65c002d36b94983177
Srushti Dhakate <86475735+srushti-dhakate-2210@users.noreply.github.com>
2023-07-08 13:59:11 +0530

    Update NavIcons.jsx

--- client/src/components/NavIcons/NavIcons.jsx
+++ client/src/components/NavIcons/NavIcons.jsx
@@ -9,14 +9,17 @@ import { Link } from "react-router-dom";
 const NavIcons = () => {
   return (
     <div className="navIcons">
+
       <Link to="../home">
         <img src={Home} alt="" />
       </Link>
+
       <UilSetting />
+
       <img src={Noti} alt="" />
-      <Link to="../chat">
-        <img src={Comment} alt="" />
-      </Link>
+
+      <img src={Comment} alt="" />
+
     </div>
   );
 };
